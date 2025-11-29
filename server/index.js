077074ad0b641c04f493a5f31a18df805943a586
nitinsehrawat020 <nitinsehrawat020@gmail.com>
2025-11-29 19:35:16 +0530

    fix: update CORS origin to support multiple allowed origins

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["content-Type", "Authorization"],
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   })
 );
 
